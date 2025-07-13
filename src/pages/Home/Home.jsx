d24f730f054101d3ccef95030e45b6e499b8e50b
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-07-13 12:55:33 +0545

    fetch all products done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,36 @@
+import axios from "axios"
 import Navbar from "../../components/navbar/Navbar"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+    const [products, setProducts] = useState([])
+    const fetchAllData = async () => {
+        const response = await axios.get("https://66e31cae494df9a478e44f01.mockapi.io/products")
+        setProducts(response.data)
+    }
+    useEffect(() => {
+        fetchAllData()
+    }, [])
+    console.log(products)
     return (
         <>
             <Navbar />
-            <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white p-4">
-                <img className="w-full h-48 object-cover rounded-xl" src="https://rocktechshop.com/wp-content/uploads/2023/03/1-2.jpg" alt="Product Image" />
-                <div className="mt-4">
-                    <h2 className="text-xl font-semibold text-gray-800">Product Name</h2>
-                    <p className="mt-2 text-gray-600 text-sm">
-                        This is a short description of the product. Highlight features or key benefits here.
-                    </p>
-                </div>
+            <div className="flex flex-wrap justify-center align-middle">
+                {
+                    products.map((product) => {
+                        return (
+                            <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white p-4">
+                                <img className="w-full h-48 object-cover rounded-xl" src={product.productImage} alt="Product Image" />
+                                <div className="mt-4">
+                                    <h2 className="text-xl font-semibold text-gray-800">{product.productName}</h2>
+                                    <p className="mt-2 text-gray-600 text-sm">
+                                        {product.productDescription}
+                                    </p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
 
         </>
